21613f3bdc415f62792c851f870b567fb5a12443
GunZ_Lap2 <gunzeux71@gmail.com>
2024-09-21 12:04:11 +0530

    Added send_email.py file

--- pages/Contact me.py
+++ pages/Contact me.py
@@ -6,7 +6,7 @@ df = pd.read_csv("Topic.csv")
 st.title("Contact me")
 
 with st.form("Send Message"):
-    st.text_input("Your E-Mail")
-    st.selectbox("Type on Enquiry :", df)
-    st.text_area("Message")
+    user_mail = st.text_input("Your E-Mail")
+    user_option = st.selectbox("Type on Enquiry :", df)
+    raw_msg = st.text_area("Message")
     st.form_submit_button("Submit")
---
+++ send_email.py
@@ -0,0 +1,24 @@
+import streamlit as st
+import os
+import smtplib
+import ssl
+
+
+def send_email(message):
+    host = "smtp@gmail.com"
+    port = 465
+
+    try:
+        password = os.getenv("GA_PAUL_PASS")
+    except Exception as ex:
+        password = st.secrets("GA_PAUL_PASS")
+
+    user_mail = "gunzeuxapp@gmail.com"
+    receiver_mail = "pauljoseph.dpj@gmail.com"
+    context = ssl.create_default_context()
+
+    with smtplib.SMTP_SSL(host, port, context=context) as server:
+        server.login(user_mail, password)
+        server.sendmail(user_mail, receiver_mail, message)
+
+    print("completed")
